e1f46707744a274d71dcbd46a89207660c9b30c5
wucheshi <wucheshi@126.com>
2018-07-21 21:35:22 +0800

    添加麦兜圈页面

--- pages/circle/index.js
+++ pages/circle/index.js
@@ -3,10 +3,58 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    // 消息列表
-    messageList: [
+    // 话题tags
+    topicTags: [
       {
-        
+        id: 0,
+        title: '全部'
+      },
+      {
+        id: 1,
+        title: '6月第二期新品试吃'
+      },
+      {
+        id: 1,
+        title: '6月第二期新品试吃'
+      },
+      {
+        id: 1,
+        title: '6月第二期新品试吃'
+      }
+    ],
+
+    // 话题中间banner
+    topicBanner: 'http://i4.bvimg.com/654779/6962858c005cf9b6.png',
+
+    // 话题列表
+    topicList: [
+      {
+        src: 'http://i4.bvimg.com/654779/b9d60932adbcde56.png',
+        title: '小味官方账号',
+        pTime: '5-18 00:00',
+        content: '上期新品故事池奖励已发放<br>@测试内容',
+        imageArr: [
+          'http://i4.bvimg.com/654779/c162cb8cbb9c15bb.png',
+          'http://i4.bvimg.com/654779/a3ef7f00a38a36e6.png',
+          'http://i4.bvimg.com/654779/a1e7a3bf74195836.png'
+        ],
+        seeNum: 0,
+        heartNum: 1,
+        messageNum: 2
+      },
+      {
+        src: 'http://i4.bvimg.com/654779/b9d60932adbcde56.png',
+        title: '小味官方账号',
+        pTime: '5-18 00:00',
+        content: '上期新品故事池奖励已发放<br>@测试内容',
+        imageArr: [
+          'http://i4.bvimg.com/654779/c162cb8cbb9c15bb.png',
+          'http://i4.bvimg.com/654779/a3ef7f00a38a36e6.png',
+          'http://i4.bvimg.com/654779/a1e7a3bf74195836.png'
+        ],
+        seeNum: 0,
+        heartNum: 1,
+        messageNum: 2
       }
     ]
   },
